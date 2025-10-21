3fc4f643f0a0e0825b4d2815894e489c0d783c6a
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-21 21:11:26 +0530

    Data loader bean added

--- src/main/java/com/aryak/springai/SpringAiApplication.java
+++ src/main/java/com/aryak/springai/SpringAiApplication.java
@@ -1,13 +1,24 @@
 package com.aryak.springai;
 
+import com.aryak.springai.rag.RandomDataLoader;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(SpringAiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(RandomDataLoader dataLoader) {
+        return args -> {
+            // do something at startup
+            dataLoader.loadDataInVectorStore();
+        };
     }
 
 }
