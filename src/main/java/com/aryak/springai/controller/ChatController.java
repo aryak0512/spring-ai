50136245848f9f3598988fe78ce16c30cc959337
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-21 15:57:13 +0530

    Prompt templates - I

--- src/main/java/com/aryak/springai/controller/ChatController.java
+++ src/main/java/com/aryak/springai/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.aryak.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,12 @@ public class ChatController {
 
     private final ChatClient ollamaClient;
     private final ChatClient openAiClient;
+
+    @Value("classpath:/promptTemplates/user_prompt_template.st")
+    private Resource userPromptTemplate;
+
+    @Value("classpath:/promptTemplates/system_prompt_template.st")
+    private Resource systemPromptTemplate;
 
     public ChatController(ChatClient ollamaClient, ChatClient openAiClient) {
         this.ollamaClient = ollamaClient;
@@ -27,5 +35,18 @@ public class ChatController {
                 .user(message)
                 .call()
                 .content();
+    }
+
+    @GetMapping("/email")
+    public String emailResponse(@RequestParam("customerName") String customerName,
+                                @RequestParam("customerMessage") String customerMessage) {
+        return openAiClient
+                .prompt()
+                .system(systemPromptTemplate) // overriding default system message from the one in config
+                .user(promptTemplateSpec ->
+                        promptTemplateSpec.text(userPromptTemplate)
+                                .param("customerName", customerName)
+                                .param("customerMessage", customerMessage))
+                .call().content();
     }
 }
